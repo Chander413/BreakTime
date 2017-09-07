5ca2fb049dea00ab4b9347c7dcef275014c77083
Chander <cnarsingoju@rapidbizapps.com>
2017-09-07 15:57:28 +0530

    Added signup view controller.

---
+++ BreakTime/BreakTime/BTSignUPVC.swift
@@ -0,0 +1,35 @@
+//
+//  BTSignUPVC.swift
+//  BreakTime
+//
+//  Created by chander on 07/09/17.
+//  Copyright © 2017 RBA. All rights reserved.
+//
+
+import UIKit
+
+class BTSignUPVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
